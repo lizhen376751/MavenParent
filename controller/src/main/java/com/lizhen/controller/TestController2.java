73a4816cdb2ce6958e474bbadaa6e86a1c22e460
lizhen <724577807@qq.com>
2018-03-09 15:00:08 +0800

    验证servlert是单例模式并且修改为线程安全

--- controller/src/main/java/com/lizhen/controller/TestController2.java
+++ controller/src/main/java/com/lizhen/controller/TestController2.java
@@ -10,6 +10,10 @@ import java.io.IOException;
  * Created by Administrator on 2018/3/9.
  */
 public class TestController2 extends HttpServlet {
+    private volatile int i= 1;
+    public TestController2(){
+        System.out.println("无参构造函数");
+    }
     @Override
     public void init() throws ServletException {
         System.out.println("servlert 初始化!!!!");
@@ -18,6 +22,13 @@ public class TestController2 extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("接受请求并且处理......");
+        synchronized (TestController2.class){
+            resp.getWriter().write("这是第几次"+i+"次请求");
+            i++;
+        }
+
+
+
     }
 
     @Override
